cd90f294e3490ebbeb82e5f6b9cb3c0ba3d65dbe
Muskan Rafiq Shaikh <muskanshaikh20004@gmail.com>
2023-09-19 00:36:45 +0530

    todolist

---
+++ index.js
@@ -0,0 +1,48 @@
+let pendingTasks=0;
+function addtask(){
+    let task = document.getElementById("input_task").value;
+     if (task.trim()!=="")
+     {
+        let table = document.querySelector(".tasktable table");
+        let newRow = table.insertRow(table.rows.length);
+
+        let cell1 = newRow.insertCell(0); 
+        let cell2 = newRow.insertCell(1); 
+        let cell3 = newRow.insertCell(2); 
+        let cell4 = newRow.insertCell(3); 
+
+        cell1.innerHTML = table.rows.length - 1;
+        cell2.innerHTML = task;
+        cell3.innerHTML = "Pending";
+        cell4.innerHTML = '<button onclick="completeTask(this)">Complete</button>';
+
+        document.getElementById("input_task").value="";
+
+        pendingTasks++;
+        displaymsg();
+     }
+    }
+    function  completeTask(button)
+    {
+        let row = button.closest("tr");
+        let status = row.cells[2];
+        status.innerHTML= "Completed";
+        let btn = row.cells[3];
+        btn.innerHTML = '<button onclick="deleteTask(this)">Delete</button>'
+
+        pendingTasks--;
+        displaymsg();
+    }
+    function deleteTask(button)
+    {
+        let row = button.closest("tr");
+        row.remove();
+
+        displaymsg();
+    }
+
+    function displaymsg()
+    {
+        let msg = document.querySelector(".displaymsg p");
+        msg.innerHTML = "You have "+pendingTasks+" task pending";
+    }
